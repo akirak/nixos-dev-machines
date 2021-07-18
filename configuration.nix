255d2839d7903a0281eaede4100b4bf24e71517f
Akira Komamura <akira.komamura@gmail.com>
2021-07-18 11:09:33 +0900

    Disable NetworkManager-wait-online to prevent a build error

--- configuration.nix
+++ configuration.nix
@@ -322,6 +322,8 @@ in
     };
   };
 
+  systemd.services.NetworkManager-wait-online.enable = false;
+
  # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
